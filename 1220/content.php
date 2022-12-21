848cc28cca2dc512d76ca1a3821be6767177a03c
Penny Ho <hopenny00@gmail.com>
2022-12-21 16:03:11 +0800

    1221

--- 1220/content.php
+++ 1220/content.php
@@ -9,26 +9,26 @@
 </head>
 
 <body>
-  <form action="#" method="" id="login_form">
+  <form action="#" method="" id="content_form">
     <h1>聯絡表單</h1>
     <p>姓名:<input type="text" name="user"></p>
     <p>電話: <input type="tel" name="tel" id=""></p>
     <p>意見回饋</p>
-    <textarea name="sugestion" cols="30" rows="5"></textarea>
+    <textarea name="suggestion" cols="30" rows="5"></textarea>
     <input type="submit" value="確認"> <input type="reset" value="取消">
   </form>
 </body>
 
 </html>
 <?php
-if (isset($_GET['user']) && isset($_GET['tel']) && isset($_GET['sugestion'])
-    && !empty($_GET['user']) && !empty($_GET['tel']) && !empty($_GET['sugestion'])) {
+if (isset($_GET['user']) && isset($_GET['tel']) && isset($_GET['suggestion'])
+    && !empty($_GET['user']) && !empty($_GET['tel']) && !empty($_GET['suggestion'])) {
     $user = $_GET["user"];
     $tel = $_GET["tel"];
-    $sugestion = $_GET["sugestion"];
+    $suggestion = $_GET["suggestion"];
 
-    echo "<script>document.getElementById('login_form').style.display = 'none'</script> <br>";
+    echo "<script>document.getElementById('content_form').style.display = 'none'</script> <br>";
     echo "姓名:{$user} <br>";
     echo "電話:{$tel} <br>";
-    echo "意見:{$sugestion} <br>";
+    echo "意見:{$suggestion} <br>";
 }
